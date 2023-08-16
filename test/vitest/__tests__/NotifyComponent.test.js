bd4b9ed5a9e1a2012fab377ebd414a3e5757ac1a
zisakmc <mishaela98@gmail.com>
2023-08-16 17:11:03 +0530

    vitest/__tests/no+.test--> async to await

--- test/vitest/__tests__/NotifyComponent.test.js
+++ test/vitest/__tests__/NotifyComponent.test.js
@@ -7,7 +7,7 @@ import NotifyComponent from './demo/NotifyComponent.vue';
 installQuasar({ plugins: { Notify } });
 
 describe('notify example', () => {
-  it('should call notify on click', async () => {
+  it('should call notify on click', await() => {
     expect(NotifyComponent).toBeTruthy();
 
     const wrapper = mount(NotifyComponent, {});
