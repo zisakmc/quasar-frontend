27bba4b7604cb3f0aa815c34571d69b61d9eb5ed
zisakmc <mishaela98@gmail.com>
2023-08-17 17:55:46 +0530

    profilepage

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,1 +1,19 @@
-<template>this profile</template>
+<template>
+  <q-card class="flex q-pa-md q-mt-lg">
+    <div class="flex justify-center q-mt-xs">
+      <q-avatar size="7em">
+        <img src="https://cdn.quasar.dev/img/avatar.png" />
+      </q-avatar>
+    </div>
+    <div class="flex-col q-pa-md">
+      <q-item-section class="text-weight-medium">
+        <table>
+          <tr>
+            <td>Name</td>
+            <td>xyc</td>
+          </tr>
+        </table>
+      </q-item-section>
+    </div>
+  </q-card>
+</template>
